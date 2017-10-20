9ee1695f32ef6205830f067d1dd45f9cae09cdf3
compsci <compsci@compsci>
2017-10-20 10:46:34 -0600

    Completed the cipher except for z wrap and index

--- src/caesar/CaesarCipher.java
+++ src/caesar/CaesarCipher.java
@@ -2,4 +2,33 @@ package caesar;
 
 public class CaesarCipher {
 	
+	private String alpha = "abcdefghijklmnopqrstuvwxyz";
+	String secretText = "";
+	public String encode(String plainText, int key) {
+		
+		System.out.println("test, alpha length = " + alpha.length());
+		System.out.println("test, first char is: " + alpha.charAt(0));
+		System.out.println("test, last char is: " + alpha.charAt(alpha.length()-1));
+		
+		for (int i = 0; i < alpha.length()-1; i++ ) {
+			
+			System.out.println(alpha.charAt(i));
+			
+		}
+		System.out.println("\n\n");
+		
+		for (int i = 0; i < plainText.length(); i++) {
+			index = alpha.indexOf(plainText.charAt(i));
+			System.out.println("index = " + index);
+			
+			keyAdded = index+key;
+			 
+			System.out.println("encoded is " + alpha);
+		}
+		
+		
+		return secretText;
+		
+	}
+	
 }

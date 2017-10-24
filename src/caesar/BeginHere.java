d739b5ec378150ab6a9a6db9c7eccfe80eef389e
compsci <compsci@compsci>
2017-10-24 10:01:24 -0600

    My method to solve spaces and upper cases

--- src/caesar/BeginHere.java
+++ src/caesar/BeginHere.java
@@ -5,13 +5,12 @@ import java.util.Scanner;
 public class BeginHere {
 
 	public static void main(String[] args) {
-		
 		Scanner kbd = new Scanner(System.in);
 		
 		CaesarCipher myCipher = new CaesarCipher();
 		
 		System.out.println("Please enter the plain text to encode:");
-		String plainText = kbd.nextLine();
+		String plainText = kbd.nextLine().toLowerCase();
 		
 		System.out.println("Please enter the key:");
 		int key = kbd.nextInt();
